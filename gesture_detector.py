18cb679eb1af8a023bca8568dfe60a35e482c116
zheng gao <1152046539@qq.com>
2021-12-20 13:53:23 +0800

    gesture detector

--- gesture_detector.py
+++ gesture_detector.py
@@ -15,8 +15,13 @@ def finger_check(landmark):
     ring = landmark[13:17]
     pinky = landmark[17:]
 
-    return [straight_finger(thumb), straight_finger(index), straight_finger(middle), 
-            straight_finger(ring), straight_finger(pinky)]
+    str_fingers = [straight_finger(thumb), straight_finger(index), straight_finger(middle), straight_finger(ring), straight_finger(pinky)]
+
+    if is_straight_finger(str_fingers, [1]):
+        if finger_up(index):
+            return 'up'
+
+    return ''
 
 def straight_finger(finger) :
     error_angle = 10
@@ -31,6 +36,23 @@ def straight_finger(finger) :
         return True
     else:
         return False
+
+def is_straight_finger(fingers, require):
+    result = False
+    for index in range(len(require)):
+        if fingers[require[index]]:
+            result = True
+    return result
+
+
+def finger_up(finger):
+    error_angle = 10
+
+    mcp,tip = finger[0], finger[3]
+    ky = (tip.y - mcp.y)/distance(mcp, tip)
+    if -ky > math.sin(math.pi*(1/2 - error_angle/180)):
+        return True
+    return False
 
 def distance(point1, point2):
     return math.sqrt(math.pow(point1.x - point2.x, 2) + math.pow(point1.y - point2.y, 2))
@@ -48,8 +70,12 @@ if __name__ == '__main__':
     
     pTime = 0
     cTime = 0
+
+    detected = 0
     
     while True:
+        detected += 1
+
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
@@ -59,12 +85,15 @@ if __name__ == '__main__':
                 str_fingers = finger_check(handLms.landmark)
                 print('new check')
                 print(str_fingers)
+                if detected%15 == 0:
+                    print('30fpes')
+                    
 
                 for id, lm in enumerate(handLms.landmark):
                     h, w, c = img.shape
                     cx, cy = int(lm.x *w), int(lm.y*h)
                     
-                    cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+                    cv2.circle(img, (cx,cy), 3, (255, 0, 255), cv2.FILLED)
     
                 mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
@@ -76,7 +105,7 @@ if __name__ == '__main__':
     
         cv2.imshow("Image", img)
     
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord('q'):
                 break
 
     cap.release()
